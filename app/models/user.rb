f7b4de0f44684f3e098df27d683b66ec98366b2b
Sean Byrd <byrdsd50@gmail.com>
2020-08-09 23:47:23 -0500

    Ensure email and username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User
   include ActiveModel::SecurePassword
   include Mongoid::Document
-  validates :username, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, length: { in: 8..20 }
   has_secure_password
 
@@ -15,6 +15,7 @@ class User
   field :active_at, type: Integer
 
   index({ username: 1 }, { unique: true, name: "username_index" })
+  index({ email: 1 }, { unique: true, name: "email_index" })
 
   def logged_in
   end
